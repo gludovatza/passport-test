f42e285fa7130d58d42546a3b09bb82cbadad735
gludovatza <attila@gludovatz.hu>
2023-06-19 12:15:08 +0200

    Article model fillable, Controller, Factory-Seeder

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,59 +7,33 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        return Article::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(Article $article)
     {
-        //
+        return $article;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Article $article)
+    public function store(Request $request)
     {
-        //
+        $article = Article::create($request->all());
+        return response()->json($article, 201);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Article $article)
     {
-        //
+        $article->update($request->all());
+
+        return response()->json($article, 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Article $article)
+    public function destroy(Request $request, Article $article)
     {
-        //
+        $article->delete();
+
+        return response()->json(null, 204);
     }
 }
