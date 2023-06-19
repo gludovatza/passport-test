b508d547fb513f7196fccf82c6a8415d0b275b13
gludovatza <attila@gludovatz.hu>
2023-06-19 12:15:43 +0200

    Api Routes: public and protected

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\Auth\ApiAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +22,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['middleware' => ['json.response']], function () {
     // ...
+    // public routes
+    Route::post('/login', [ApiAuthController::class,'login'])->name('login.api');
+    Route::post('/register',[ApiAuthController::class,'register'])->name('register.api');
+
+    // protected routes
+    Route::middleware('auth:api')->group(function () {
+        Route::post('/logout', [ApiAuthController::class,'logout'])->name('logout.api');
+
+        Route::get('articles', [ArticleController::class,'index']);
+        Route::get('articles/{article}', [ArticleController::class,'show']);
+        Route::post('articles', [ArticleController::class,'store']);
+        Route::put('articles/{article}', [ArticleController::class,'update']);
+        Route::delete('articles/{article}', [ArticleController::class,'delete']);
+    });
+
+
 });
+
